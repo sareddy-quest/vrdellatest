79066e118db4453952f57f25df848ea7c1032712
sareddy-quest <110263284+sareddy-quest@users.noreply.github.com>
2022-12-26 11:35:36 -0800

    Imported from Quest

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import questTheme from 'src/MyDesignSystemLightTheme';
+import ActivityList from 'src/components/ActivityList/ActivityList';
+
+function App() {
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={questTheme}>
+        <Routes>
+          <Route path="/" element={<ActivityList />} />
+        </Routes>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+}
+
+export default App;
